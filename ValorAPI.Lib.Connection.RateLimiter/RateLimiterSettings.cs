90d31c3984d1440b1d882d31eec0f7ae438b7b30
Erik Asís <e.asis@identtia.com>
2020-10-12 09:02:13 +0200

    Splitted extension methods from RateLimiter

--- ValorAPI.Lib.Connection.RateLimiter/RateLimiterSettings.cs
+++ ValorAPI.Lib.Connection.RateLimiter/RateLimiterSettings.cs
@@ -93,37 +93,4 @@ namespace ValorAPI.Lib.Connection.RateLimiter
             }
         }
     }
-
-    public static class ClientExtension
-    {
-        public static void SetRateLimiter(this Client client, RateLimiterSettings rateLimiterSettings)
-        {
-            if (client == null)
-            {
-                throw new ArgumentException("You must supply a valid client", nameof(client));
-            }
-
-            if (rateLimiterSettings == null)
-            {
-                throw new ArgumentException("You must supply a valid RateLimiterSettings", nameof(rateLimiterSettings));
-            }
-
-            rateLimiterSettings.EnableRateLimiter(client);
-        }
-
-        public static void nRateLimiter(this Client client, RateLimiterSettings rateLimiterSettings)
-        {
-            if (client == null)
-            {
-                throw new ArgumentException("You must supply a valid client", nameof(client));
-            }
-
-            if (rateLimiterSettings == null)
-            {
-                throw new ArgumentException("You must supply a valid RateLimiterSettings", nameof(rateLimiterSettings));
-            }
-
-            rateLimiterSettings.DisableRateLimiter(client);
-        }
-    }
 }
