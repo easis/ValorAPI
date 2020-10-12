25c1a7383ff4606d6b3157d04a415cbc5d1b4a21
Erik Asís <e.asis@identtia.com>
2020-10-12 08:58:29 +0200

    Added GameName constants

---
+++ ValorAPI.Lib.Data/Constant/GameName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValorAPI.Lib.Data.Constant
+{
+    public class GameName
+    {
+        public static string VALORANT = "val";
+        public static string RIOT = "riot";
+        public static string LOL = "lol";
+        public static string TFT = "tft";
+        public static string LOR = "lor";
+    }
+}
