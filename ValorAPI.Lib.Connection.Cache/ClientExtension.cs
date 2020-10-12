9edf1f715ddd6c1a543ce2c8e027e458e9d0641a
Erik Asís <e.asis@identtia.com>
2020-10-12 09:01:39 +0200

    Added a GetAsyncUncached extension method

--- ValorAPI.Lib.Connection.Cache/ClientExtension.cs
+++ ValorAPI.Lib.Connection.Cache/ClientExtension.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using ValorAPI.Lib.Connection.Event;
+using ValorAPI.Lib.Data.DTO;
+using ValorAPI.Lib.Data.Endpoint;
 
 namespace ValorAPI.Lib.Connection.Cache
 {
@@ -23,7 +27,7 @@ namespace ValorAPI.Lib.Connection.Cache
             client.EnableCache();
         }
 
-        public static async void EnableCache(this Client client)
+        public static void EnableCache(this Client client)
         {
             client.BeforeRequestAsync += OnBeforeRequestAsync;
             client.CompletedRequestAsync += OnCompletedRequestAsync;
@@ -35,32 +39,37 @@ namespace ValorAPI.Lib.Connection.Cache
             client.CompletedRequestAsync -= OnCompletedRequestAsync;
         }
 
+        public static async Task<T> GetAsyncUncached<T>(this Client client, IEndpoint endpoint) where T : IResponse
+        {
+            client.DisableCache();
+            var response = await client.GetAsync<T>(endpoint);
+            client.EnableCache();
+
+            return response;
+        }
         private static async Task OnBeforeRequestAsync(object sender, ClientRequestEventArgs e)
         {
+            var client = sender as Client;
             var clientRequest = e as ClientRequestEventArgs;
 
             using var cacheContext = new CacheContext();
             var item = cacheContext
                         .CacheItems
-                        .Where(i => i.Url == clientRequest.Url)
-                        .Select(i => i.Content)
-                        .AsSingleQuery();
-
+                        .Where(i => i.Url.ToLower() == clientRequest.Url.ToLower())
+                        .Select(i => i.Content);
 
             var content = item.FirstOrDefault();
-            if(content == null)
+            if (content == null)
             {
                 return;
             }
 
-            //var contentText = await Compression.DecompressAsync(content);
             var contentText = StringCompressor.DecompressString(content);
             clientRequest.ResponseContent = contentText;
         }
 
         private static async Task OnCompletedRequestAsync(object sender, ClientRequestEventArgs clientRequest)
         {
-            //var content = await Compression.CompressAsync(clientRequest.ResponseContent);
             var content = StringCompressor.CompressString(clientRequest.ResponseContent);
 
             var cacheItem = new CacheItem()
